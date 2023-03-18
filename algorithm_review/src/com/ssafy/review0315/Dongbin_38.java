8002b3b2679625a0005b74541c5c7f2bca166c7a
gkatldus1 <gkatldus1@gmail.com>
2023-03-18 20:26:32 +0900

    algorithm-study

--- algorithm_review/src/com/ssafy/review0315/Dongbin_38.java
+++ algorithm_review/src/com/ssafy/review0315/Dongbin_38.java
@@ -15,8 +15,45 @@ public class Dongbin_38 {
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
 		
+		for(int i=0; i<501; i++) {
+			Arrays.fill(graph[i], INF);
+		}
 		
+		for(int a=1; a<=n; a++) {
+			for(int b=1; b<=n; b++) {
+				if(a==b) graph[a][b] = 0;
+			}
+		}
 		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			graph[a][b] = 1;
+		}
+		
+		for(int k=1; k<=n; k++) {
+			for(int a=1; a<=n; a++) {
+				for(int b=1; b<=n; b++) {
+					graph[a][b] = Math.min(graph[a][b], graph[a][k]+graph[k][b]);
+				}
+			}
+		}
+		
+		int result = 0;
+		for(int i=1; i<=n; i++) {
+			int cnt = 0;
+			for(int j=1; j<=n; j++) {
+				if(graph[i][j]!=INF || graph[j][i]!=INF) {
+					cnt += 1;
+				}
+			}
+			if(cnt == n) {
+				result += 1;
+			}
+		}
+		
+		System.out.println(result);
 		
 	}
 	
