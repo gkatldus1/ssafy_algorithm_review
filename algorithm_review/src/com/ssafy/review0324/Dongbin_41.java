50a901ed46d7e22d304cbd5fefec4c6b4254b891
gkatldus1 <gkatldus1@gmail.com>
2023-03-25 17:49:42 +0900

    algorithm-study

---
+++ algorithm_review/src/com/ssafy/review0324/Dongbin_41.java
@@ -0,0 +1,66 @@
+package com.ssafy.review0324;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Dongbin_41 {
+	public static int n, m;
+	public static int[] parent = new int[501];
+	
+	public static int findParent(int x) {
+		if(x == parent[x])	return x;
+		return parent[x] = findParent(parent[x]);
+	}
+	
+	public static void unionParent(int a, int b) {
+		a = findParent(a);
+		b = findParent(b);
+		if(a < b) parent[b] = a;
+		else parent[a] = b;
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		for(int i=1; i<=n; i++) {
+			parent[i] = i;
+		}
+		
+		for(int i=0; i<n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j=0; j<n; j++) {
+				int x = Integer.parseInt(st.nextToken());
+				if(x == 1) {
+					unionParent(i+1, j+1);
+				}
+			}
+		}
+		
+		ArrayList<Integer> plan = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<m; i++) {	
+			int x = Integer.parseInt(st.nextToken());
+			plan.add(x);
+		}
+			
+		boolean result = true;
+		for(int i=0; i<m-1; i++) {
+			if(findParent(plan.get(i))!= findParent(plan.get(i+1))) {
+				result = false;
+				break;
+			}
+		}
+		
+		if(result) System.out.println("YES");
+		else System.out.println("NO");
+		
+	}
+	
+	
+	
+	
+}
