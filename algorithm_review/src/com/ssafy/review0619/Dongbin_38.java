5ef678cbde6e1f09c6e933b242df47afef894c61
gkatldus1 <gkatldus1@gmail.com>
2023-06-19 21:03:48 +0900

    algorithm-study

--- algorithm_review/src/com/ssafy/review0619/Dongbin_38.java
+++ algorithm_review/src/com/ssafy/review0619/Dongbin_38.java
@@ -1,11 +1,13 @@
-package com.ssafy.review0618;
+package com.ssafy.review0619;
 
 import java.util.*;
 
-public class Dongbin_37 {
+public class Dongbin_38 {
 	public static final int INF = (int) 1e9;
+	
 	public static int n, m;
-	public static int[][] graph = new int[101][101];
+	
+	public static int[][] graph = new int[501][501];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -13,22 +15,20 @@ public class Dongbin_37 {
 		n = sc.nextInt();
 		m = sc.nextInt();
 		
-		for(int i=0; i<101; i++) {
+		for(int i=0; i<501; i++) {
 			Arrays.fill(graph[i], INF);
 		}
 		
 		for(int a=1; a<=n; a++) {
 			for(int b=1; b<=n; b++) {
-				if(a==b) graph[a][b] = 0;
+				if(a == b) graph[a][b] = 0; 
 			}
 		}
 		
 		for(int i=0; i<m; i++) {
 			int a = sc.nextInt();
-			int b = sc.nextInt();
-			int c = sc.nextInt();
-			
-			if(c < graph[a][b]) graph[a][b] = c;
+			int b= sc.nextInt();
+			graph[a][b] = 1;
 		}
 		
 		for(int k=1; k<=n; k++) {
@@ -39,21 +39,20 @@ public class Dongbin_37 {
 			}
 		}
 		
-		for(int a=1; a<=n; a++) {
-			for(int b=1; b<=n; b++) {
-				if(graph[a][b] == INF) {
-					System.out.print(0 + " ");
-				}
-				else {
-					System.out.print(graph[a][b] + " ");
+		int result = 0;
+		for(int i=1; i<=n; i++) {
+			int cnt = 0;
+			for(int j=1; j<=n; j++) {
+				if(graph[i][j] != INF || graph[j][i] != INF) {
+					cnt += 1;
 				}
 			}
-			System.out.println();
+			if(cnt == n) {
+				result += 1;
+			}
 		}
-		
+		System.out.println(result);
 		
 	}
-	
-	
 	
 }
